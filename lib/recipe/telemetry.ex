135bb3bf5a7d5904a59bddc8a88df6cc8576b46c
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2017-10-20 12:02:53 +0100

    Apply Elixir 1.6 formatter on entire codebase

--- lib/recipe/telemetry.ex
+++ lib/recipe/telemetry.ex
@@ -16,23 +16,23 @@ defmodule Recipe.Telemetry do
   @doc """
   Invoked at the start of a recipe execution.
   """
-  @callback on_start(Recipe.t) :: :ok
+  @callback on_start(Recipe.t()) :: :ok
 
   @doc """
   Invoked at the end of a recipe execution, irrespectively of
   the success or failure of the last executed step.
   """
-  @callback on_finish(Recipe.t) :: :ok
+  @callback on_finish(Recipe.t()) :: :ok
 
   @doc """
   Invoked after successfully executing a step.
   """
-  @callback on_success(Recipe.step, Recipe.t, elapsed_microseconds) :: :ok
+  @callback on_success(Recipe.step(), Recipe.t(), elapsed_microseconds) :: :ok
 
   @doc """
   Invoked after failing to execute a step.
   """
-  @callback on_error(Recipe.step, term, Recipe.t, elapsed_microseconds) :: :ok
+  @callback on_error(Recipe.step(), term, Recipe.t(), elapsed_microseconds) :: :ok
 
   defmacro __using__(_opts) do
     quote do
