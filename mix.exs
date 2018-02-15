06ac8993ddff5f6895e5a640fef0352ba322201f
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2018-02-15 19:38:54 +0000

    Bump Elixir requirements

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule Recipe.Mixfile do
       version: @version,
       description: @description,
       package: package(),
-      elixir: "~> 1.4",
+      elixir: "~> 1.5",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       source_url: "https://github.com/cloud8421/recipe",
