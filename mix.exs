14ed8dd5a840f48538503b5166b81cd4ee96c991
Claudio Ortolina <cloud8421@gmail.com>
2025-08-09 16:42:47 +0300

    Cleanup unused dependencies

--- mix.exs
+++ mix.exs
@@ -21,8 +21,6 @@ defmodule Recipe.Mixfile do
       source_url: "https://github.com/cloud8421/recipe",
       homepage_url: "https://github.com/cloud8421/recipe",
       docs: [main: "readme", extras: ["README.md"]],
-      test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: [coveralls: :test],
       deps: deps()
     ]
   end
@@ -34,9 +32,7 @@ defmodule Recipe.Mixfile do
   defp deps do
     [
       {:ex_doc, "~> 0.18.1", only: :docs, runtime: false},
-      {:inch_ex, "~> 0.5.6", only: :docs, runtime: false},
       {:credo, "~> 0.8.1", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.8.1", only: :test, runtime: false},
       {:dialyxir, "~> 0.5.0", only: :dev, runtime: false}
     ]
   end
